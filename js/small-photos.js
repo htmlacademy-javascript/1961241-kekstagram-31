0dee7b0e0d9ef2fa39c3254c0d2fe53f361e3604
SergeiU <heartofkerrigan@gmail.com>
2024-03-24 17:33:21 +0900

    добавляет jsdoc и DRY

--- js/small-photos.js
+++ js/small-photos.js
@@ -1,16 +1,18 @@
-
-
 const variousListPhoto = document.querySelector('.pictures');
 const variousPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-
+/**
+ * Добавляет массив фотографий
+ * @param {Array} photos Массив фотографий
+ */
 const addPhotos = (photos) => {
   const variousListFragment = document.createDocumentFragment();
 
   photos.forEach(({url, description, likes, comments}) => {
     const photoElement = variousPhotoTemplate.cloneNode(true);
-    photoElement.querySelector('.picture__img').src = url;
-    photoElement.querySelector('.picture__img').alt = description;
+    const imageElement = document.querySelector('.picture__img');
+    imageElement.src = url;
+    imageElement.alt = description;
     photoElement.querySelector('.picture__likes').textContent = likes;
     photoElement.querySelector('.picture__comments').textContent = comments;
     variousListPhoto.append(photoElement);
