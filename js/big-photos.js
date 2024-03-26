1e8a4cdd77b4acd69a1bf65b8526f846f38c6c66
SergeiU <heartofkerrigan@gmail.com>
2024-03-27 01:03:15 +0900

    добавляет big-photos.js module8-task1

--- js/big-photos.js
+++ js/big-photos.js
@@ -1,2 +1,80 @@
+import { isEscapeKey } from './util.js';
+
+const bodyElement = document.querySelector('body');
+const overlayElement = document.querySelector('.overlay');
 const fullPhotoOpenElement = document.querySelector('.big-picture');
 const fullPhotoCloseElement = document.querySelector('.big-picture__cancel');
+
+const onPhotoKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFullPhoto();
+  }
+};
+
+/**
+ * открывает большое фото
+ * @param {String} url ссылка
+ * @param {Number} likes количество лайков
+ * @param {String} description описание фото
+ */
+function openFullPhoto ({url, likes, description}) {
+  overlayElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  fullPhotoOpenElement.querySelector('img').src = url;
+  fullPhotoOpenElement.querySelector('.likes-count').textContent = likes;
+  fullPhotoOpenElement.querySelector('.social__caption').textContent = description;
+
+  document.addEventListener('keydown', onPhotoKeydown);
+}
+
+/**
+ * закрывает большое фото
+ */
+function closeFullPhoto () {
+  overlayElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onPhotoKeydown);
+}
+
+fullPhotoOpenElement.addEventListener('click', () => {
+  openFullPhoto();
+});
+
+fullPhotoCloseElement.addEventListener('click', () => {
+  closeFullPhoto();
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    overlayElement.classList.add('hidden');
+  }
+});
+
+/**
+ * добавляет обработчик клика по фото
+ * @param {Array} photos массив фотографий
+ * @param {Object} photo объект с данными фотографии
+ */
+const addPhotosClickHandler = (photos, photo) => {
+  photos.addEventListener('click', () => {
+    openFullPhoto(photo);
+    document.addEventListener('keydown', onPhotoKeydown);
+  });
+};
+
+/**
+ * связывает данные из массива
+ * @param {Array} photos массив фотографий
+ */
+const addListeners = (photos) => {
+  const photoElements = document.querySelectorAll('.picture');
+
+  for (let i = 0; i < photoElements.length; i++) {
+    addPhotosClickHandler(photoElements[i], photos[i]);
+  }
+};
+
+export {addListeners};
--- js/small-photos.js
+++ js/small-photos.js
@@ -10,14 +10,14 @@ const addPhotos = (photos) => {
 
   photos.forEach(({url, description, likes, comments}) => {
     const photoElement = variousPhotoTemplate.cloneNode(true);
-    const imageElement = document.querySelector('.picture__img');
+    const imageElement = photoElement.querySelector('img');
     imageElement.src = url;
     imageElement.alt = description;
     photoElement.querySelector('.picture__likes').textContent = likes;
     photoElement.querySelector('.picture__comments').textContent = comments;
     variousListPhoto.append(photoElement);
   });
-  variousListPhoto.appendChild(variousListFragment);
+  variousListPhoto.append(variousListFragment);
 };
 
 export {addPhotos};
