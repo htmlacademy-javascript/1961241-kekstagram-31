583713f986fad5d702d90d7244f489ee78c7b725
SergeiU <heartofkerrigan@gmail.com>
2024-04-10 00:33:22 +0900

    поправка

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ import { generatePhotos } from './data.js';
 import { addPhotos } from './small-photos.js';
 import { addListeners } from './big-photos.js';
 import { applyScale, uploadPhoto } from './upload-form-photo.js';
-import { initSlider } from './effect-photo.js';
+import { applySlider } from './effect-photo.js';
 
 const variousPhotos = generatePhotos();
 addPhotos(variousPhotos);
 addListeners(variousPhotos);
 uploadPhoto();
 applyScale();
-initSlider();
+applySlider();
 
